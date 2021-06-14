e1cd342f220ea58b628908dc0e0dd5ac1bce527d
Harry Wang <harryjwang@gmail.com>
2021-06-14 16:46:06 -0400

    first version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# mini-data-science
-A Minimalist End-to-End Data Science Tutorial
+# About
+
+This repo contains the code for my minimalist end-to-end data science project tutorial.
+
+## Setup
+
+Python 3 required, see my tutorial to setup Python 3: https://bit.ly/2uX6wAX
+
+Clone the repo, go to the repo folder, setup the virtual environment, and install the required packages:
+
+
+```shell
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+Run `$ jupyter lab` to go over the notebooks.
+
+## Data Science App
+
+Steps to get the streamlit app running (make sure to use the Terminal and the virtual environment is activated):
+
+1. Get the data and model files ready
+2. Create a notebook to do analysis and prediction
+3. Create an app python file based on the notebook, such as `titanic-streamlit-app.py`
+4. Run the app locally (Local URL: http://localhost:8501) using terminal: `streamlit run titanic-streamlit-app.py` 
+5. Stop the app by using ctrl + C or closing the terminal
+6. Deploy the app to the cloud for public access via services such as streamlit share, heroku, aws by following my tutorial at https://github.com/harrywang/streamlit-basics. you can see an example at: https://st-demo-harrywang.herokuapp.com/
---
+++ titanic-streamlit-app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import joblib
+
+st.title('Titanic Survival Analysis and Prediction')
+# load dataset
+df = pd.read_csv('titanic.csv')
+
+# show the entire dataframe
+st.write(df)
+
+# f-string
+st.subheader('Survival Rate')
+survival_count = df['Survived'].value_counts()
+st.text(f'Survival rate = {survival_count.values[1]/sum(survival_count):.2%}')
+
+# simple plotting
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+survival_count.plot.bar(ax=ax[0])
+df['Age'].plot.hist(ax=ax[1])
+st.pyplot(fig)
+
+
+st.subheader('Making Prediction')
+st.markdown('**Please provide passenger information**:')  # you can use markdown like this
+
+
+# load models
+tree_clf = joblib.load('clf-best.pickle')
+
+# get inputs
+
+sex = st.selectbox('Sex', ['female', 'male'])
+age = int(st.number_input('Age:', 0, 120, 20))
+sib_sp = int(st.number_input('# of siblings / spouses aboard:', 0, 10, 0))
+par_ch = int(st.number_input('# of parents / children aboard:', 0, 10, 0))
+pclass = st.selectbox('Ticket class (1 = 1st, 2 = 2nd, 3 = 3rd)', [1, 2, 3])
+fare = int(st.number_input('# of parents / children aboard:', 0, 100, 0))
+embarked = st.selectbox('Port of Embarkation (C = Cherbourg, Q = Queenstown, S = Southampton)', ['C', 'Q', 'S'])
+
+# this is how to dynamically change text
+prediction_state = st.markdown('calculating...')
+
+passenger = pd.DataFrame(
+    {
+        'Pclass': [pclass],
+        'Sex': [sex], 
+        'Age': [age],
+        'SibSp': [sib_sp],
+        'Parch': [par_ch],
+        'Fare': [fare],
+        'Embarked': [embarked],
+    }
+)
+
+y_pred = tree_clf.predict(passenger)
+
+if y_pred[0] == 0:
+    msg = 'This passenger is predicted to be: **died**'
+else:
+    msg = 'This passenger is predicted to be: **survived**'
+
+prediction_state.markdown(msg)
